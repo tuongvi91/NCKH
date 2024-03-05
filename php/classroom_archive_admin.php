ad8ec19ddcbff6b9299e8fed8395e70d46260ed8
tuongvi91 <tuongvits77@gmail.com>
2024-03-05 23:36:40 +0700

    reup classroom_archive_admin

--- php/classroom_archive_admin.php
+++ php/classroom_archive_admin.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Lớp học lưu trữ</title>
+    <title>Danh sách lớp học</title>
     <link rel="stylesheet" href="../css/classroom_archive_admin.css">
     <?php
     include("../php/sidebar_admin.php");
