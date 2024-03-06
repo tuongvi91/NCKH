dd7b3e0de1279990c5c84dc493f840f888a13278
tuongvi91 <tuongvits77@gmail.com>
2024-03-06 22:38:27 +0700

    handling the display of classroom list

--- php/card_class_teacher.php
+++ php/card_class_teacher.php
@@ -10,29 +10,68 @@
     ?>
 </head>
 <body>
-    <section class="home-section">
+    <form action="" method = "get">
         <div class="search-container">
-            <input type="text" class="search-input" placeholder="Nhập lớp cần tìm ...">
-        </div>
+            <input type="search" name ="search" class="search-input" placeholder="Nhập lớp cần tìm ...">
+            <input type = "submit" name = "OK" value="OK" style="display: none";>
+        </div>        
+    </form>       
          <div class="add-class-button-container">
             <button class="add-class-button">Thêm lớp học</button>
         </div>
-        <div class="card-container">
-            <a href="link-to-your-page-1" class="card card-1">
-                <div class="card-info">
-                    <div class="card-time">6/11/2024</div>
-                    <h1 class="card-title">Lập trình WEB</h1>
-                </div>
-            </a>
-            <a href="link-to-your-page-2" class="card card-2">
-                <div class="card-info">   
-                    <div class="card-time">10/07/2023</div>
-                    <h1 class="card-title">Tin học cơ bản</h1>
-                </div>
-            </a>
+    <?php
+        require 'connect.php';
+        $userid = $_SESSION['manguoidung'];
+        
+        $maLop = array(); 
+        $ma = array();
+        //hiển thị các lớp
+        $laymalop = "select*from teaching_schedule where user_id = '".$userid."' ";
+        $rs= mysqli_query($conn, $laymalop);
+        while ($row= mysqli_fetch_assoc($rs))
+        {
+            $maLop[] = $row['course_id'];
+        }
+        //loại bỏ các lớp trùng lặp
+        $ma = array_map(function($item) use (&$ma) {
+            if (!in_array($item, $ma)) {
+                $ma[] = $item;
+                return $item;
+            }
+        }, $maLop);
+        
+        foreach ($ma as $malop)
+        {
+        //lấy các lớp có mã do gvien này dạy
+        if (isset($_GET['OK']) && !empty($_GET['OK']))
+        {
+            $key = $_GET['search'];    
+            $laythongtin = "select*from course where course_id = '".$malop."' and name like '%key%'";
+            $kq = mysqli_query($conn, $laythongtin);            
+        }
+        else 
+        {
+            $laythongtin = "select*from course where course_id = '".$malop."'";
+            $kq = mysqli_query($conn, $laythongtin);
+        }
+        while  ($r = mysqli_fetch_array($kq)) 
+        {          
 
+    ?>
+    <section class="home-section">        
+        <div class="card-container">
+            <a href="#" class="card card-1">
+                <div class="card-info">
+                    <div class="card-time"><?php echo $r['start_time'] ?></div>
+                    <h1 class="card-title"><?php echo $r['name'] ?></h1>
+                </div>
+            </a>
         </div>
-        <!-- Form thêm lớp học -->
+    <?php 
+        }
+    }
+    ?>
+          <!-- Form thêm lớp học -->
         <div class="add-class">
 
             <header>
