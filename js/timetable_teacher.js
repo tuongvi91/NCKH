588e9e69072d3a793dc47ab0d220deb3bb5d534e
tuongvi91 <tuongvits77@gmail.com>
2024-03-15 23:14:36 +0700

    add drag and drop for subject items

--- js/timetable_teacher.js
+++ js/timetable_teacher.js
@@ -16,3 +16,23 @@ document.addEventListener("DOMContentLoaded", function () {
         document.body.innerHTML = originalContents;
     }
 });
+
+let lists = document.getElementsByClassName("list");
+let right =document.getElementById("right");
+let left=document.getElementById("left");
+for(list of lists) {
+    list.addEventListener("dragstart",function(e){
+        let selected = e.target;
+        right.addEventListener("dragover", function(e){ e.preventDefault();
+        });
+        right.addEventListener("drop", function(e){ right.appendChild(selected);
+        selected=null;
+    });
+    left.addEventListener("dragover", function(e){ e.preventDefault();
+    });
+    left.addEventListener("drop", function(e){
+    left.appendChild(selected);
+    selected=null;
+});
+});
+}
